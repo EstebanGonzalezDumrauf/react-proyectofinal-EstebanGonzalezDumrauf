4909d48b1fc3782e6b76c6110117b8fcc2e61031
Esteban Gonzalez Dumrauf <esteban.a.gonzalez.d@gmail.com>
2023-06-09 13:11:19 -0300

    commit con proyecto final de react

--- src/components/Error404.jsx
+++ src/components/Error404.jsx
@@ -1,9 +1,9 @@
 const Error404 = () => {
     return (
-        <div className="container my-5">
+        <div className="container-fluid my-5">
             <div className="row">
                 <div className="col text-center">
-                    <div class="alert alert-danger" role="alert">
+                    <div className="alert alert-danger" role="alert">
                         <h1>Error 404</h1>
                         <h3>La página no existe.</h3>
                     </div>
--- src/index.js
+++ src/index.js
@@ -3,6 +3,25 @@ import ReactDOM from 'react-dom/client';
 import App from './App';
 import './index.css';
 
+// Import the functions you need from the SDKs you need
+import {
+    initializeApp
+} from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+    apiKey: "AIzaSyBA4kiz89b9XUXQmsZpj4nZsnjPWc42epo",
+    authDomain: "shining-life-gonzalezd.firebaseapp.com",
+    projectId: "shining-life-gonzalezd",
+    storageBucket: "shining-life-gonzalezd.appspot.com",
+    messagingSenderId: "270320737621",
+    appId: "1:270320737621:web:871e980103d6158659e3dc"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(<App />);
-
